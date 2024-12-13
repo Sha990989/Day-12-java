29cf9cf52296289d42cc2e58fbaccce9e53892e9
Sha990989 <anavaneethasankar@gmail.com>
2024-12-13 15:26:29 +0530

    Add files via upload

---
+++ mapinterface2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class mapinterface2 {
+    public static void main(String args[])
+    {
+            HashMap<String,String> a=new HashMap<>();
+            System.out.println(a);
+        }
+        
+    }
+    
+
