461388b2a13b3146c144f3837f77cfaef8035d88
Sha990989 <anavaneethasankar@gmail.com>
2024-12-13 21:32:36 +0530

    Add files via upload

---
+++ hashmapusingforeach.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class hashmapusingforeach{
+public static void main(String args[])
+{
+    HashMap<String,String> a=new HashMap<>();
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+        String S=sc.next();
+        String S1=sc.next();
+        a.put(S,S1); 
+    }
+    for(String S : a.keySet())
+    {
+        System.out.println(S+":"+a.get(S));
+    }
+}
+}
