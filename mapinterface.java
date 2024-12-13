7341aef6f0267dadd7fda03a284cf649877dbf85
Sha990989 <anavaneethasankar@gmail.com>
2024-12-13 15:09:36 +0530

    Add files via upload

---
+++ mapinterface.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class mapinterface {
+    public static void main(String args[])
+    {
+        HashMap<String,String> a=new HashMap<>();
+        a.put("SNO","01");
+        a.put("SNAME","Muraja");
+        System.out.println(a);
+    }
+    
+}
